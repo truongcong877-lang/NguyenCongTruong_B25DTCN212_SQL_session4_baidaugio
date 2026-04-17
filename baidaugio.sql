da491f1c9a12fe7e00817daae351c7d1e1f9868a
truongcong877-lang <truongcong877@gmail.com>
2026-04-17 07:30:09 +0700

    Add files via upload

---
+++ baidaugio.sql
@@ -0,0 +1,46 @@
+create database ShopManager;
+use ShopManager;
+
+create table Categories(
+   category_id int primary key auto_increment,
+   category_name varchar(100)
+);
+
+create table Products(
+   product_id int primary key auto_increment,
+   product_name varchar(100),
+   price decimal(18, 2) check(price >0),
+   stock int check(stock >0),
+   category_id int,
+   foreign key (category_id) references Categories (category_id)
+);
+
+insert into Categories (category_name)
+values 
+('Điện tử'),
+('Thời trang');
+
+insert into Products (product_name, price, stock, category_id)
+values
+('iPhone 15', 25000000, 10, 1),
+('Samsung S23', 20000000, 5, 1),
+('Áo sơ mi nam', 500000, 50, 2),
+('Giày thể thao', 12000000, 20,2);
+
+set SQL_SAFE_UPDATES =0;
+
+update Products set price = 26000000
+where product_name = 'iPhone 15';
+
+update Products set stock = stock + 10;
+
+delete from Products
+where product_id = 4;
+
+delete from Products
+where price < 1000000;
+
+select * from Products;
+
+select * from Products 
+where stock > 15;
